6a1a0045d9f6c3ccd440bce8279ab5b7087f425e
Omkar Mali <omkarmali7376@gmail.com>
2026-02-08 15:01:05 +0530

    06/02/2026-Customer Logic of validation shirt and pant

--- middleware/pantMeasurValidation.js
+++ middleware/pantMeasurValidation.js
@@ -1,8 +1,6 @@
 const Joi = require('joi')
 
 const pantMeasurValidation = Joi.object({
-    PantQnt : Joi.number().integer().required(),
-    amount : Joi.number().required(),
     outsideLength: Joi.number().required(),
     insideLength : Joi.number().required(),
     rise : Joi.number().required(),
--- middleware/shirtMeasurValidation.js
+++ middleware/shirtMeasurValidation.js
@@ -1,16 +1,17 @@
 const Joi = require('joi')
 
 const shirtMeasurValidation = Joi.object({
-    shirtQnt : Joi.number().integer().required(),
-    amount : Joi.number().required(),
     length: Joi.number().required(),
-    chest : Joi.number().required(),
-    shoulder : Joi.number().required(),
-    sleeve : Joi.number().required(),
-    collar : Joi.number().required(),
-    front1 : Joi.number().required(),
-    front2 : Joi.number().required(),
-    front3 : Joi.number().required()
+    chest: Joi.number().required(),
+    shoulder: Joi.number().required(),
+    sleeve: Joi.number().required(),
+    collor: Joi.number().required(),
+    cuff: Joi.number().required(),
+    back: Joi.number().required(),
+    bicep: Joi.number().required(),
+    front1: Joi.number().required(),
+    front2: Joi.number().required(),
+    front3: Joi.number().required()
 })
 
-module.exports = {shirtMeasurValidation}
+module.exports = { shirtMeasurValidation }
