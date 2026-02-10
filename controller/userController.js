2f643ec8f430a438ac6de2076f1cac86d2c768bd
Omkar Mali <omkarmali7376@gmail.com>
2026-02-10 16:45:35 +0530

    10/02/2026-Restoring DB

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,8 @@
 const { Op, where, QueryTypes } = require("sequelize");
-const {User} = require("../model/users/userModel")
-const {validate, func} = require("joi");
+const { User } = require("../model/users/userModel")
+const { validate, func } = require("joi");
 const { userSchema } = require("../middleware/userValidate");
 const { sequelize } = require("../config/dbConnect");
-const { Profile } = require("../model/users/profileModel");
 const bcrypt = require("bcrypt");
 const jsonwebtoken = require('jsonwebtoken');
 
@@ -114,7 +113,7 @@ const finderUse = async(req, res) => {
     return res.json(data, created)
 } */
 
-const createUser = async (req, res) =>{
+const createUser = async (req, res) => {
     try {
         const userData = req.body;
 
@@ -126,71 +125,65 @@ const createUser = async (req, res) =>{
             return res.status(409).json({ message: "Email already exists" });
         }
 
-        const {error} = userSchema.validate(userData)
-        if(error) return res.status(400).json({message : error.details[0].message})
+        const { error } = userSchema.validate(userData)
+        if (error) return res.status(400).json({ message: error.details[0].message })
 
         const { password, ...otherUserFields } = userData;
         const hashedPass = await bcrypt.hash(password, 10)
         const user = await User.create({
-            ...otherUserFields,         
+            ...otherUserFields,
             password: hashedPass
         })
-        const profile = await Profile.create({userId : user.id})
         return res.status(201).json({
             user: [{
-                USER_ID : user.id,
-                USERNAME : user.username,
-                NAME : user.name,
-                AGE : user.age,
-                MOB_NO : user.mob_no,
-                EMAIL : user.email,
-                ADDRESS : user.address,
-                ISACTIVE : user.isActive  
-            }], 
-            profile: [{
-                USER_ID : profile.userId,
-                ID : profile.id,
-                BIO : profile.bio
-            }]}
-        )
+                USER_ID: user.id,
+                USERNAME: user.username,
+                NAME: user.name,
+                AGE: user.age,
+                MOB_NO: user.mob_no,
+                EMAIL: user.email,
+                ADDRESS: user.address,
+                ISACTIVE: user.isActive
+            }]
+        })
     } catch (e) {
-        console.error(e)        
-        return res.status(404).json({message : e.message})
+        console.error(e)
+        return res.status(404).json({ message: e.message })
     }
 }
 
 const login = async (req, res) => {
-    try{
-        const user = await User.findOne({where: {email: req.body.email}})
-        if(!user){
+    try {
+        const user = await User.findOne({ where: { email: req.body.email } })
+        if (!user) {
             return res.status(401).json({
                 message: "Auth Failed"
             })
         } else {
-            bcrypt.compare(req.body.password, user.password, function(err, result) {
-                if(result){
+            bcrypt.compare(req.body.password, user.password, function (err, result) {
+                if (result) {
                     const token = jsonwebtoken.sign({
                         email: user.email,
                         userId: user.id
-                    }, process.env.SECRET, {expiresIn: '1d'}
-                )
+                    }, process.env.SECRET, { expiresIn: '1d' }
+                    )
 
-                return res.status(200).json({
-                    code: 200,
-                    message: "Auth Successful", 
-                    data: [{
-                        token: token,
-                        userData: [{
-                            USER_ID: user.id,
-                            USERNAME: user.username,
-                            NAME: user.name,
-                            EMAIL_ID: user.email,
-                            MOBILE_NO: user.mob_no,
-                            ADDRESS: user.address,
-                            ISACTIVE: user.isActive
+                    return res.status(200).json({
+                        code: 200,
+                        message: "Auth Successful",
+                        data: [{
+                            token: token,
+                            userData: [{
+                                USER_ID: user.id,
+                                USERNAME: user.username,
+                                NAME: user.name,
+                                EMAIL_ID: user.email,
+                                MOBILE_NO: user.mob_no,
+                                ADDRESS: user.address,
+                                ISACTIVE: user.isActive
+                            }]
                         }]
-                    }]
-                })
+                    })
                 } else {
                     return res.status(401).json({
                         message: "Auth failed"
@@ -198,14 +191,14 @@ const login = async (req, res) => {
                 }
             })
         }
-    }catch (error) {
+    } catch (error) {
         console.error(error)
-        return res.status(500).json({message : error.message})
+        return res.status(500).json({ message: error.message })
     }
-    
+
 }
 
-const getUsers = async(req, res) =>{
+const getUsers = async (req, res) => {
     try {
         // const users = await User.findAll()
 
@@ -218,30 +211,30 @@ const getUsers = async(req, res) =>{
         //         model: Profile, as: "profile"
         //     }
         // })
-        return res.status(200).json({users})
+        return res.status(200).json({ users })
     } catch (error) {
         console.error(error)
-        return res.status(500).json({message : error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
-const getUserByID = async(req, res) =>{
+const getUserByID = async (req, res) => {
     try {
         const user = await User.findByPk(req.params.id)
-        return res.status(200).json({user})
+        return res.status(200).json({ user })
     } catch (error) {
         console.error(error)
-        return res.status(500).json({message : error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
-const updateUser = async(req, res) =>{
+const updateUser = async (req, res) => {
     try {
         // const {error} = userSchema.validate(req.body)
         // if(error) return res.status(400).json({message : error.details[0].message})
 
         const user = await User.findByPk(req.params.id)
-        if(!user ) return res.status(500).json({error : "user not found"})
+        if (!user) return res.status(500).json({ error: "user not found" })
 
         await user.update({
             username: req.body.username,
@@ -251,38 +244,38 @@ const updateUser = async(req, res) =>{
             mob_no: req.body.mob_no,
             address: req.body.address,
         });
-        return res.status(200).json({msg: "User Updated"})
-    } catch(error) {
+        return res.status(200).json({ msg: "User Updated" })
+    } catch (error) {
         console.error(error)
-        return res.status(500).json({error : error.message})
+        return res.status(500).json({ error: error.message })
     }
 }
 
-const updatePassword = async(req, res) =>{
+const updatePassword = async (req, res) => {
     try {
         const user = await User.findByPk(req.params.id)
-        if(!user ) return res.status(500).json({error : "user not found"})
+        if (!user) return res.status(500).json({ error: "user not found" })
 
         const hashedPass = await bcrypt.hash(req.body.password, 10)
-        await user.update({password: hashedPass});
-        
-        return res.status(200).json({msg: "Password Updated"})
-    } catch(error) {
+        await user.update({ password: hashedPass });
+
+        return res.status(200).json({ msg: "Password Updated" })
+    } catch (error) {
         console.error(error)
-        return res.status(500).json({error : error.message})
-    }   
+        return res.status(500).json({ error: error.message })
+    }
 }
 
-const deleteUser = async(req, res) =>{
+const deleteUser = async (req, res) => {
     try {
         const user = await User.findByPk(req.params.id)
-        if(!user ) return res.status(500).json({error : "user not found"})
+        if (!user) return res.status(500).json({ error: "user not found" })
 
         await user.destroy();
-        return res.status(200).json({msg: "User Deleted"})    
-    } catch(error) {
+        return res.status(200).json({ msg: "User Deleted" })
+    } catch (error) {
         console.error(error)
-        return res.status(500).json({error : error.message})
+        return res.status(500).json({ error: error.message })
     }
 }
 
